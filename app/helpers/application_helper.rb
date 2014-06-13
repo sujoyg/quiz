a0451c7724af3779b8895eb8cb3e0757d3b8763b
Sujoy Gupta <sujoy.gupta@appdirect.com>
2014-06-13 01:10:49 -0700

    Enabling HTML editor on the new and edit questions page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def set_css(*sources)
+    content_for(:css, sources.join(",") + ",")
+  end
+
+  def set_javascript(*sources)
+    content_for(:javascript, sources.join(",") + ",")
+  end
+
+  def page_css
+    if content_for?(:css)
+      sources = content_for(:css).chomp(",").split(",")
+      stylesheet_link_tag *sources
+    end
+  end
+
+  def page_javascript
+    if content_for?(:javascript)
+      sources = content_for(:javascript).chomp(",").split(",")
+      javascript_include_tag *sources
+    end
+  end
 end
